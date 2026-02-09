bb37b2287f42d28b9e9cad01f2ec40b2026ab44d
Ikkaku <hrkfji221@gmail.com>
2026-02-09 21:58:44 +0900

    Initial Commit

---
+++ ARHandWorld/ARHandWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARHandWorldApp.swift
+//  ARHandWorld
+//
+//  Created by 藤井陽樹 on 2026/02/09.
+//
+
+import SwiftUI
+
+@main
+struct ARHandWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
